4427d3c823bbecb83d7b7589a1ed7a473a9adc50
DhanishaK02 <146560034+DhanishaK02@users.noreply.github.com>
2026-07-09 16:05:39 +0530

    Add sockMerchant function for counting pairs
    Implemented sockMerchant function to count sock pairs.

--- Warmup/01_Sales_by_Match/solution.py
+++ Warmup/01_Sales_by_Match/solution.py
@@ -1,1 +1,17 @@
+def sockMerchant(n, ar):
+    # Dictionary to store frequency of each sock color
+    freq = {}
 
+    # Count frequency of each color
+    for color in ar:
+        if color in freq:
+            freq[color] += 1
+        else:
+            freq[color] = 1
+
+    # Count pairs
+    pairs = 0
+    for count in freq.values():
+        pairs += count // 2
+
+    return pairs
